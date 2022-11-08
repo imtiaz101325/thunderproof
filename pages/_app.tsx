33c53c21e20e46448a6f5db5630a004bf49b47bf
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2022-11-08 19:06:09 +0100

    Adds initial setup
    - adds packages
    - adds fonts and styling support
    - modifies page setup
    - adds initial content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import { Roboto_Flex } from "@next/font/google";
+import reset from "styled-reset";
+
+import theme from "../theme";
+import Head from "next/head";
+
+const roboto = Roboto_Flex({
+  subsets: ["latin"],
+  axes: ["wdth"],
+  variable: "--roboto-font",
+});
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  html {
+    font-family: ${roboto.style.fontFamily}
+  }
+`;
+
+const Container = styled.main`
+  min-height: 100vh;
+  min-width: 100vw;
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>ThunderProof Blog</title>
+          <meta name="description" content="A blog for learning about STB app development in Lightning.js" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <nav></nav>
+
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+
+        <footer></footer>
+      </ThemeProvider>
+    </>
+  );
 }
